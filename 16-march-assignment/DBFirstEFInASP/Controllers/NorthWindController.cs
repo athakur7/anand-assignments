0cd51b86ed794aebbe00f3ae6f9a6b775006aef2
athakur7 <anandthakur178@gmail.com>
2026-03-16 11:02:16 +0530

    EF and previous work DogApp

---
+++ 16-march-assignment/DBFirstEFInASP/Controllers/NorthWindController.cs
@@ -0,0 +1,25 @@
+﻿using DBFirstEFInASP.Data;          // make sure this matches your actual namespace
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DBFirstEFInASP.Controllers
+{
+    public class NorthWindController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SpainCustomers()
+        {
+            using var cnt = new NorthwindContext();
+
+            var spainCustomers = cnt.Customers
+                .Where(x => x.Country == "Spain")
+                .ToList();
+
+            return View(spainCustomers);
+        }
+    }
+}
